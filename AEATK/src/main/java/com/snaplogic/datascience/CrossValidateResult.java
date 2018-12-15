748291a1f9951f488c00df6888f5129887398029
Sanchai W <swasantayanan@snaplogic.com>
2018-12-16 00:15:22 +0700

    Add temporary result store

---
+++ AEATK/src/main/java/com/snaplogic/datascience/CrossValidateResult.java
@@ -0,0 +1,22 @@
+package com.snaplogic.datascience;
+
+public class CrossValidateResult {
+    private double matricValue;
+    private String crossValidationString;
+
+    public double getMatricValue() {
+        return matricValue;
+    }
+
+    public void setMatricValue(double matricValue) {
+        this.matricValue = matricValue;
+    }
+
+    public String getCrossValidationString() {
+        return crossValidationString;
+    }
+
+    public void setCrossValidationString(String crossValidationString) {
+        this.crossValidationString = crossValidationString;
+    }
+}
---
+++ AEATK/src/main/java/com/snaplogic/datascience/ValidationResultStore.java
@@ -0,0 +1,34 @@
+package com.snaplogic.datascience;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ValidationResultStore {
+
+    private static class ValidationResultStoreLoader {
+        private static final ValidationResultStore INSTANCE = new ValidationResultStore();
+    }
+
+    public static ValidationResultStore getInstance(){
+        return ValidationResultStoreLoader.INSTANCE;
+    }
+
+    ValidationResultStore(){
+        runResultMap = new ConcurrentHashMap<>();
+    }
+
+    private ConcurrentHashMap<String, RunResultHistory> runResultMap;
+
+    public RunResultHistory getRunResultHistory(String runKey){
+        RunResultHistory runResultHistory = runResultMap.get(runKey);
+        if(runResultHistory != null){
+            runResultHistory = new RunResultHistory();
+            runResultMap.put(runKey, runResultHistory);
+        }
+
+        return runResultHistory;
+    }
+
+    public RunResultHistory pollRunResultHistory(String runKey){
+        return runResultMap.remove(runKey);
+    }
+}
